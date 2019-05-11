94ff15599a3e8adec7c09a392de311a39cbaea0c
Martulla <martulla@wp.pl>
2019-05-11 15:32:46 +0200

    first part od exercises 3 - model implemented

--- twitter/models.py
+++ twitter/models.py
@@ -1,3 +1,21 @@
+import datetime
+
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+ #długości wyrzucone do zmiennych, na wypadek, gdyby miałyby być użyte w innych miejscach. Wygodniej będzie je zmienić.
+
+TWITTER_MAXIMUM_TWEET_LENGTH = 280
+TWITTER_MAXIMUM_COMMENT_LENGTH = 60
+TWITTER_MAXIMUM_MSG_LENGTH = 64
+
+
+class Tweet(models.Model):
+    content = models.CharField(max_length=TWITTER_MAXIMUM_TWEET_LENGTH)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '[{}] TWEET by {}: {}'.format(self.creation_date, self.author, self.content[:20])
