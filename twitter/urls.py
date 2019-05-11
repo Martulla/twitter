b84a65ce4da9ebdbb6b448cb98e86e051797d323
Martulla <martulla@wp.pl>
2019-05-11 15:11:36 +0200

    create main page

---
+++ twitter/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from twitter import views #nie musimy importować konkretnych nazw, bo przez nazwą widoku jest views.
+
+app_name = 'twitter'
+urlpatterns = [
+    path('', views.MainWebpageView.as_view(), name = 'index'),
+]
